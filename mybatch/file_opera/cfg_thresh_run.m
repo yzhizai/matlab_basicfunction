b581936d7532e3dfc5f6a6a2c9d06f7faa22cfc2
Shaofeng Duan <duansf@ihep.ac.cn>
2015-06-09 10:35:00 +0800

    this is  a complete edition for thresh

--- mybatch/file_opera/cfg_thresh_run.m
+++ mybatch/file_opera/cfg_thresh_run.m
@@ -1,23 +1,52 @@
 function out = cfg_thresh_run(job)
 
-mymenu = job.mymenu;
+fidnames = fieldnames(job.mymenu);
 filenames = job.input_file;
 
-mymask = load(mask_file{1});
-if isfield(mymask,'sig_mat_temp')
-    S = substruct('.','sig_mat_temp');
-    mask = subsref(mymask,S);
-else
-    error('the mat files should have the ''sig_mat_temp'' fields');
-end
+
 out = cell(size(filenames));
-for aa = 1:numel(filenames)
-    [path tit ext] = fileparts(filenames{aa});
-    CM = load(filenames{aa});
-    CM = CM.*mask;
-    fname = fullfile(path,['m' tit ext]);
-    dlmwrite(fname,CM)
-    out{aa} = fname;
+switch fidnames{:}
+    case 'branch1'
+        mask_file = job.mymenu.branch1.mask;
+        mymask = load(mask_file{1});
+        if isfield(mymask,'sig_mat_temp')
+             S = substruct('.','sig_mat_temp');
+             mask = subsref(mymask,S);
+        else
+            error('the mat files should have the ''sig_mat_temp'' fields');
+        end
+
+        for aa = 1:numel(filenames)
+            [path tit ext] = fileparts(filenames{aa});
+            CM = load(filenames{aa});
+            S1 = substruct('.','CM');
+            CM = subsref(CM,S1);
+            fname = fullfile(path,['m' tit ext]);
+            save(fname,CM)
+            out{aa} = fname;
+        end
+    case 'branch2'
+        thresh_type = job.mymenu.branch2.thresh_type;
+        thresh_val = job.mymenu.branch2.thresh_val;
+        for aa = 1:numel(filenames)
+            [path tit ext] = fileparts(filenames{aa});
+            CM = load(filenames{aa});
+            S1 = substruct('.','CM');
+            CM = subsref(CM,S1);
+            switch thresh_type
+                case 'relative'
+                    CM = threshold_proportional(CM,thresh_val);
+                case 'absolute'
+                    CM = threshold_absolute(CM,thresh_val);
+                otherwise
+                        error('You should input right format for "relative" and "absolute"');
+            end
+            fname = fullfile(path,['m' tit ext]);
+            save(fname,CM)
+            out{aa} = fname;
+        end
 end
+
+
 
     
